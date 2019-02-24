6f3030f6c15ea803bf639be59b75cf86e7ba77b9
ShaSta_57 <44626281+Staten-40@users.noreply.github.com>
2019-02-23 19:01:32 -0800

    push for Jennifer
    Concat <li> foreach

--- oop_game_php_starter/inc/Phrase.php
+++ oop_game_php_starter/inc/Phrase.php
@@ -24,8 +24,7 @@ class Phrase
            //Split screen into lowercase chracters
         $characters = str_split(strtolower($this->currentPhrase));
         $output = '<ul>';
-        $output .= '<li class="hide letter h">h</li>';
-        $output .= '<li class="hide letter h">h</li>';
+       /* $output .= '<li class="hide letter h">h</li>';
         $output .= '<li class="hide letter o">o</li>';
         $output .= '<li class="hide letter w">w</li>';
         $output .= '<li class="hide space "> . " " .  </li>';
@@ -35,18 +34,23 @@ class Phrase
         $output .= '<li class="hide space"> . " " . </li>';
         $output .= '<li class="hide letter y">y</li>';
         $output .= '<li class="hide letter o">o</li>';
-        $output .= '<li class="hide letter u">u</li>';
+        $output .= '<li class="hide letter u">u</li>';*/
 
         $output .= '<ul>';
         return $output;
 
-            foreach($charcters as $char) {
-        
-
+            foreach($characters as $char) {
+                if($char == " ") {
+                   $output .= '<li class="hide space "> . " " .  </li>';
+                } else {
+                   $output .= '<li class="hide letter $char">' . $char . '</li>';
+                    }
+                }   
+                
             }
-                   
-   }
-    
-}
+     }                  
+        
+   
+
 
 //var_dump($phrase);
