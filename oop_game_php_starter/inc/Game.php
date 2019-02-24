d75d97c1ff1d5bfb1b2184885782008863d80799
ShaSta_57 <44626281+Staten-40@users.noreply.github.com>
2019-02-24 14:22:27 -0800

    Counting back the lives

--- oop_game_php_starter/inc/Game.php
+++ oop_game_php_starter/inc/Game.php
@@ -54,6 +54,23 @@ class Game
    
 }
 
+function displayScore($lives) {
+    for($lives; $lives<=5, $lives--);
+
+    '<div id="scoreboard" class="section">
+    <ol>
+        <li class="tries"><img src="images/loseHeart.png" height="35px" widght="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" widght="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" widght="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" widght="30px"></li>
+        <li class="tries"><img src="images/liveHeart.png" height="35px" widght="30px"></li>
+    </ol>
+</div>';
+    
+}
+
+
+    
 
 
 
